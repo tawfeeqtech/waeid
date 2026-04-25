821333f9accccc7cce50d84b892510cfc58aeef3
ghayatech <ghayatech.media@gmail.com>
2026-04-25 09:21:28 +0300

    Remove @tailwindcss/line-clamp plugin - now included by default in v3.3+
    - Fixes Tailwind CSS warning
    - Plugin is now built-in to Tailwind CSS core

--- tailwind.config.js
+++ tailwind.config.js
@@ -150,6 +150,5 @@ module.exports = {
     plugins: [
       require('@salla.sa/twilight-tailwind-theme'),
       require('@tailwindcss/forms'),
-      require('@tailwindcss/line-clamp'),
     ],
 }
